39bbbe1ed9059b37a28c86c96599361d1f70ff53
porpoise864 <oertlys@oregonstate.edu>
2020-04-24 15:49:44 -0400

    Initial commit

---
+++ min-max.py
@@ -0,0 +1,23 @@
+# Author: Sarah Oertly
+# Date: 10/15/2019
+# Description: Write a program that asks the user how many integers they would like to enter.  You can assume they will
+# enter an integer >= 1.  The program will then prompt the user to enter that many integers.  After all the numbers
+# have been entered, the program should display the largest and smallest of those numbers (no, you cannot use arrays,
+# or any other material we haven't covered).  When you run your program it should match the following format:
+number = int(input("How many integers would you like to enter"))
+i = 0
+print("Please enter", number, "integers.")
+maxi = 0
+mini = 0
+while number != maxi:
+    i > maxi
+    i < mini
+
+    if i < maxi:
+        i += 1
+        maxi = i
+        print("max: ", maxi)
+        if i > mini:
+            i += 1
+            mini = i
+        print("min: ", mini)
